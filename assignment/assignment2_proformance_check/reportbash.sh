4ed59c0ae5368bc166c66f2cacfcc39150072fb9
Rajendra Singh <singh.raj1997@gmail.com>
2019-02-24 11:17:53 +0530

    added graph visualisation

--- assignment/assignment2_proformance_check/reportbash.sh
+++ assignment/assignment2_proformance_check/reportbash.sh
@@ -6,8 +6,8 @@ thread_list=(1 2 4 10 20 50 100)
 
 echo -n "" > report/simple/simple.txt
 echo -n "" > report/busywait/busywait.txt
-echo -n "" > report/mutex/mutex_simple.txt
-echo -n "" > report/mutex/mutex_overhead.txt
+echo -n "" > report/mutex_simple/mutex_simple.txt
+echo -n "" > report/mutex_overhead/mutex_overhead.txt
 
 for i in ${iter_list[*]}
 do
@@ -15,16 +15,23 @@ do
 	do
 		echo -n "iters, NUM_THREAD, value, Execution time = [" ${i} ", " ${n} ", ">> report/simple/simple.txt
 		echo -n "iters, NUM_THREAD, value, Execution time = [" ${i} ", " ${n} ", ">> report/busywait/busywait.txt
-        echo -n "iters, NUM_THREAD, value, Execution time = [" ${i} ", " ${n} ", ">> report/mutex/mutex_simple.txt
-		echo -n "iters, NUM_THREAD, value, Execution time = [" ${i} ", " ${n} ", ">> report/mutex/mutex_overhead.txt
+        echo -n "iters, NUM_THREAD, value, Execution time = [" ${i} ", " ${n} ", ">> report/mutex_simple/mutex_simple.txt
+		echo -n "iters, NUM_THREAD, value, Execution time = [" ${i} ", " ${n} ", ">> report/mutex_overhead/mutex_overhead.txt
 		./compute_mutex_simple ${i} ${n} >> report/simple/simple.txt
 		./compute_mutex_overhead ${i} ${n} >> report/busywait/busywait.txt
-		./compute_mutex_simple ${i} ${n} >> report/mutex/mutex_simple.txt
-		./compute_mutex_overhead ${i} ${n} >> report/mutex/mutex_overhead.txt
+		./compute_mutex_simple ${i} ${n} >> report/mutex_simple/mutex_simple.txt
+		./compute_mutex_overhead ${i} ${n} >> report/mutex_overhead/mutex_overhead.txt
     done
 	echo -n "."
 done
 echo ""
 echo "Report successfully create in /report folder"
+echo "Creating graph wait..."
+python show_graph.py report/simple/simple.txt
+python show_graph.py report/busywait/busywait.txt
+python show_graph.py report/mutex_simple/mutex_simple.txt
+python show_graph.py report/mutex_overhead/mutex_overhead.txt
+echo "Graph successfully create in /report folder"
+
 echo "--------------------------------------------------------------------------"
 echo ""
--- assignment/assignment2_proformance_check/show_graph.py
+++ assignment/assignment2_proformance_check/show_graph.py
@@ -1,13 +1,66 @@
-#print("hi\n")
-
-f = open("report/simple/simple.txt", "r")
+import sys
+import matplotlib.pyplot as plt
+filename = sys.argv[1]
+f = open(filename, "r")
 data = f.read()
-#print(type(data), len(data))
 splitdata = data.split("\n")
-#print(splitdata)
 l = len(splitdata) - 1
-#print("l=",l)
 splitdata = splitdata[:l]
-#print(((splitdata[0].split("[")[1]).split("]")[0]).split(","))
-x = [float(b.strip()) for b in ((splitdata[0].split("[")[1]).split("]")[0]).split(",")]
-print("x=", x)
+tuplelist = []
+for ele in splitdata:
+    t = [float(b.strip()) for b in ((ele.split("[")[1]).split("]")[0]).split(",")]
+    tuplelist.append([t[0],t[1],t[3]])
+
+unique_iter = []
+for ele in tuplelist:
+    if ele[0] not in unique_iter:
+        unique_iter.append(ele[0])
+
+unique_no_thread = []
+for ele in tuplelist:
+    if ele[1] not in unique_no_thread:
+        unique_no_thread.append(ele[1])
+
+unique_iter_x = []
+unique_iter_y = []
+for ele1 in unique_iter:
+    templistx = []
+    templisty = []
+    for ele2 in tuplelist:
+        if ele1 == ele2[0]:
+            templistx.append(ele2[1])
+            templisty.append(ele2[2])
+    unique_iter_x.append(templistx)
+    unique_iter_y.append(templisty)
+
+unique_no_thread_x = []
+unique_no_thread_y = []
+for ele1 in unique_no_thread:
+    templistx = []
+    templisty = []
+    for ele2 in tuplelist:
+        if ele1 == ele2[1]:
+            templistx.append(ele2[0])
+            templisty.append(ele2[2])
+    unique_no_thread_x.append(templistx)
+    unique_no_thread_y.append(templisty)
+
+plt.figure(0)
+tstr = "".join([str(i) for i in range(len(unique_iter))])
+for i,ele in enumerate(unique_iter):
+    plt.plot(unique_iter_x[i],unique_iter_y[i],label="Const_iter="+str(unique_iter[i]).split(".")[0] )
+    plt.title("Using "+filename.split("/")[1]+ " method, and "+"for const iter=" + str(unique_iter[i]))
+    plt.ylabel('cpu_time')
+    plt.xlabel('Num_thread')
+    plt.legend(tstr)
+    plt.savefig('report/'+filename.split("/")[1] + '/'+'const_iter_'+str(unique_iter[i]).split(".")[0]+'.png')
+
+plt.figure(1)
+tstr = "".join([str(i) for i in range(len(unique_no_thread))])
+for i,ele in enumerate(unique_no_thread):
+    plt.plot(unique_no_thread_x[i],unique_no_thread_y[i] )
+    plt.title("Using "+filename.split("/")[1]+ " method, and "+"for const NUM_THREAD=" + str(unique_no_thread[i]))
+    plt.ylabel('cpu_time')
+    plt.xlabel('Num_iters')
+    plt.legend(tstr)
+    plt.savefig('report/'+filename.split("/")[1] + '/'+'const_thread_'+str(unique_no_thread[i]).split(".")[0]+'.png')
